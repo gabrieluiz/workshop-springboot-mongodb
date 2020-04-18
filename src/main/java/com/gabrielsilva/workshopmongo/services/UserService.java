33057e7970d8466c8ddf79c77f8eb447297376de
Gabriel Silva <gabriell_alves@live.com>
2020-04-18 10:03:56 -0300

    Conecting to MongoDB - repository and service

---
+++ src/main/java/com/gabrielsilva/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.gabrielsilva.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielsilva.workshopmongo.domain.User;
+import com.gabrielsilva.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+
+}
